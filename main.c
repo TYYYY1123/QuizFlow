569a0e375265f5a3215b0ef9a74a40086d166728
TYYYY1123 <f9sknzkyhh@privaterelay.appleid.com>
2026-09-14 21:04:19 +0800

    add menu-based question import and fix UTF-8 output

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "quiz.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 
 int main()
 {
+    #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
 
     int choice;
 
@@ -15,7 +22,8 @@ int main()
 
 
     printf("1.开始刷题\n");
-    printf("2.退出系统\n");
+    printf("2.导入题库\n");
+    printf("3.退出系统\n");
 
 
     printf("请选择：");
@@ -24,18 +32,23 @@ int main()
 
 
 
-    if(choice==1)
-    {
-
-        startQuiz();
-
-    }
-    else
-    {
-
-        printf("退出系统\n");
-
-    }
+    if (choice == 1)
+{
+    startQuiz();
+}
+else if (choice == 2)
+{
+    printf("正在导入题库，请稍候……\n");
+    system("python importer/import_questions.py");
+}
+else if (choice == 3)
+{
+    printf("退出系统\n");
+}
+else
+{
+    printf("输入有误，请输入1、2或3。\n");
+}
 
 
     return 0;
